a178e0ea7c856eda50123e01c60b93084362fd78
Stephen M Braudo <braudman@gmail.com>
2019-04-14 16:30:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,9 @@
 # code your #position_taken? method here!
 
-<<<<<<< HEAD
-def (position_taken?(board, index))
-=======
+
+
 def position_taken?(board, index)
->>>>>>> 0a9b8db5b71de85ab260e7d962ce97c7a34b6cd9
+
   
   if (board[index] == " " || board[index] = "" || board[index] = nil)
     puts "Free Space"
